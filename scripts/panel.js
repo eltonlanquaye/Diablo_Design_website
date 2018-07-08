8298f82c06c224da44327e3cc67a3a655cd0f6a0
EC Lanquaye <eltonlanquaye@gmail.com>
2018-07-08 01:08:31 +0000

    Panel 200

--- scripts/panel.js
+++ scripts/panel.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 
     var plusSign = $(".plus");
     var link = $(".social-media-links");
-    var wrapper = $(".wrapper");
+   
 
     plusSign.click(function(){
 
-        // $(wrapper).slideToggle(500);
-        $(link).slideToggle(500);
+        $(link).slideToggle(700);
+
     });
 
 });
