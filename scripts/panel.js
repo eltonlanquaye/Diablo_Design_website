582a7025fc8243dc5efdb745ca4b9c9386ffdd58
EC Lanquaye <eltonlanquaye@gmail.com>
2018-07-10 14:07:59 +0000

    Coded the animation of link

--- scripts/panel.js
+++ scripts/panel.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
     var plusSign = $(".plus");
     var link = $(".social-media-links");
+    var body = $("body");
+    var video = $("video");
    
 
     plusSign.click(function(){
@@ -13,3 +15,13 @@ $(document).ready(function(){
     });
 
 });
+
+
+// var videoDiv = getElementsByTagName("video");
+// var body = getElementsByTagName("body");
+
+// const pauseIt = function(){
+//     body.click(function(){
+//         video.pause();
+//     });
+// }
