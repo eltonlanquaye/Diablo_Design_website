6f0c670407b3974ee9275988f290b9363feb3103
EC Lanquaye <eltonlanquaye@gmail.com>
2018-07-10 15:34:27 +0000

    Coded for the background vid

--- scripts/video.js
+++ scripts/video.js
@@ -1,8 +1,17 @@
 
 // Play Video on load
 
-$("document").ready(function(){
-
-    // $("video").play();
-
-});
+(function() {
+    /**
+     * Video element
+     * @type {HTMLElement}
+     */
+    var video = document.getElementsByTagName("video");
+  
+    /**
+     * Check if video can play, and play it
+     */
+    video.addEventListener( "canplay", function() {
+      video.play();
+    });
+  })();
